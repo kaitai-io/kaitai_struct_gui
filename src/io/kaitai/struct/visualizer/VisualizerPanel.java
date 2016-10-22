7b46a0d944663c3e2384c150bb76197bac30c183
Mikhail Yakshin <greycat@altlinux.org>
2016-10-22 23:43:37 +0300

    More refactoring: separated tree model creation vs root node creations + start of exploration; added attrStart and attrEnd extraction from KaitaiStruct

--- src/io/kaitai/struct/visualizer/VisualizerPanel.java
+++ src/io/kaitai/struct/visualizer/VisualizerPanel.java
@@ -17,16 +17,21 @@ import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.ExpandVetoException;
 import javax.swing.tree.TreePath;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class VisualizerPanel extends JPanel {
     private JTree tree;
+    private DefaultTreeModel model;
     private HexLib hexEditor;
     private JSplitPane splitPane;
 
     private KaitaiStruct struct;
+    private Map<String, Integer> attrStart;
+    private Map<String, Integer> attrEnd;
 
     public VisualizerPanel() throws IOException {
         super();
@@ -41,26 +46,8 @@ public class VisualizerPanel extends JPanel {
         JScrollPane treeScroll = new JScrollPane(tree);
 
         splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, treeScroll, hexEditor);
-    }
 
-    public void loadAll(String dataFileName, String ksyFileName) {
-        try {
-            loadStruct(parseFileWithKSY(ksyFileName, dataFileName));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void loadStruct(KaitaiStruct struct) throws IOException {
-        this.struct = struct;
-
-        struct._io().seek(0);
-        byte[] buf = struct._io().readBytesFull();
-        hexEditor.setByteContent(buf);
-
-        DataNode root = new DataNode(0, struct, null, "[root]");
-
-        final DefaultTreeModel model = new DefaultTreeModel(root);
+        model = new DefaultTreeModel(null);
         tree.setShowsRootHandles(true);
         tree.addTreeWillExpandListener(new TreeWillExpandListener() {
             @Override
@@ -77,6 +64,24 @@ public class VisualizerPanel extends JPanel {
             }
         });
         tree.setModel(model);
+    }
+
+    public void loadAll(String dataFileName, String ksyFileName) {
+        try {
+            parseFileWithKSY(ksyFileName, dataFileName);
+            loadStruct();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadStruct() throws IOException {
+        struct._io().seek(0);
+        byte[] buf = struct._io().readBytesFull();
+        hexEditor.setByteContent(buf);
+
+        DataNode root = new DataNode(0, struct, null, "[root]");
+        model.setRoot(root);
         root.explore(model /*, progressListener */, null);
     }
 
@@ -116,20 +121,23 @@ public class VisualizerPanel extends JPanel {
         return InMemoryJavaCompiler.compile(DEST_PACKAGE + "." + className, javaSrc);
     }
 
-    private static KaitaiStruct parseFileWithKSY(String ksyFileName, String binaryFileName) throws Exception {
+    private void parseFileWithKSY(String ksyFileName, String binaryFileName) throws Exception {
         String javaSrc = compileKSY(ksyFileName);
         Class<?> ksyClass = compileAndLoadJava(javaSrc);
 
         // Find and run "fromFile" helper method to
         Method fromFileMethod = ksyClass.getMethod("fromFile", String.class);
         Object kso = fromFileMethod.invoke(null, binaryFileName);
-        KaitaiStruct ks = (KaitaiStruct) kso;
+        struct = (KaitaiStruct) kso;
 
         // Find and run "_read" that does actual parsing
         // TODO: wrap this in try-catch block
         Method readMethod = ksyClass.getMethod("_read");
-        readMethod.invoke(ks);
+        readMethod.invoke(struct);
 
-        return ks;
+        Field fAttrStart = ksyClass.getDeclaredField("_attrStart");
+        attrStart = (Map<String, Integer>) fAttrStart.get(struct);
+        Field fAttrEnd = ksyClass.getDeclaredField("_attrStart");
+        attrEnd = (Map<String, Integer>) fAttrEnd.get(struct);
     }
 }
