057a61b77fae6983beddc5c617502c9dc3489217
Peter Froud <pfroud@erp-power.com>
2022-03-04 14:11:23 -0800

    add command-line usage message

--- src/main/java/io/kaitai/struct/visualizer/MainWindow.java
+++ src/main/java/io/kaitai/struct/visualizer/MainWindow.java
@@ -9,26 +9,43 @@ public class MainWindow extends JFrame {
     private static final String APP_NAME = "Kaitai Struct Visualizer";
     private static final String VERSION = "0.8";
 
-    private VisualizerPanel vis;
+    private final VisualizerPanel visualizerPanel;
 
     public MainWindow() throws IOException {
         super(APP_NAME + " v" + VERSION);
-        vis = new VisualizerPanel();
+        visualizerPanel = new VisualizerPanel();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().add(vis.getSplitPane());
+        getContentPane().add(visualizerPanel.getSplitPane());
         pack();
         setVisible(true);
     }
 
-    public static void main(final String arg[]) throws Exception {
+    public static void main(final String[] args) throws Exception {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        MainWindow mw = new MainWindow();
+        MainWindow mainWindow = new MainWindow();
 
 
-        if (arg.length == 2) {
-            String binaryFileToParse = arg[0];
-            String ksyFileName = arg[1];
-            mw.vis.loadAll(new ByteBufferKaitaiStream(binaryFileToParse), ksyFileName);
+        switch (args.length) {
+            case 0:
+                // just show the GUI window
+                break;
+            case 2:
+                String binaryFileToParse = args[0];
+                String ksyFileName = args[1];
+                if (!ksyFileName.endsWith(".ksy")) {
+                    System.err.println("Warning: the second argument does not have file extension .ksy.");
+                    System.err.println("Command-line usage: java -jar binaryFileToParse ksyFileName");
+                }
+                try {
+                    mainWindow.visualizerPanel.loadAll(new ByteBufferKaitaiStream(binaryFileToParse), ksyFileName);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            default:
+                System.out.println("Command-line usage: java -jar binaryFileToParse ksyFileName");
+                break;
         }
+
     }
 }
